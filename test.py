07df4ce5d8563a4cb5bfcb565d066ce48ef6f977
Broderick Gardner <--global>
2015-08-07 14:55:44 -0600

    Final working commit, with comments and working keyboard exit of listen mode

--- test.py
+++ test.py
@@ -3,18 +3,32 @@
 import time
 import serial
 import sys
-import struct
-import collections as cs
-import binascii as b
-import copy
-import readline
-import ast
 import platform
 import glob
+import select
+import threading
+import readchar
 
+
+#*************************************************************
+#   
+#   The important example code is in the 'listen()' function,
+#   plus the serial port initialization near the beginning.  
+#   
+#   You may need to pip install pyserial
+#   I included the 'readchar' package because it was handy,
+#   but it can also be install via pip.
+#   
+#   You will need to plug in a BDL to see the serial ports
+#   It should be cross platform with the enum_serial_ports()
+#   function, but it was developed and tested on linux.  
+#   
+#*************************************************************
+
+#Function to find all valid serial ports on current system
 def enum_serial_ports():
     system_name = platform.system()
-    if(system_name == "Windows"):
+    if(system_name == "Windows"): #... Windows
         available = []
         for i in range(256):
             try:
@@ -24,60 +38,76 @@ def enum_serial_ports():
             except serial.SerialException:
                 pass
         return available
-    elif(system_name == "Darwin"):
+    elif(system_name == "Darwin"):  #mac
         return glob.glob('/dev/tty.usb*')
-    else:
+    else: #assume linux
         return glob.glob('/dev/ttyUSB*')
 
-#This allows inline changing loaded config by filling
-#in the input prompt with editable lines
-def rlinput(prompt, prefill=''):
-   readline.set_startup_hook(lambda: readline.insert_text(prefill))
-   try:
-      return input(prompt)
-   finally:
-      readline.set_startup_hook()
+def read_serial(ser):
+    out = ''
+#Read out a maximum of 100 bytes from device.  
+#Double while loops ensure that after reading 
+#   all available bytes that it waits .2 seconds
+#   and tries again before giving up and outputting
+    i = 100
+    while ser.inWaiting() > 0 and i > 0:
+        while ser.inWaiting() > 0 and i > 0:
+            byt = ser.read(1)
+            out += byt.decode('ascii')
+            i -= 1
+        time.sleep(.2)
 
-#This class exists to store the device config read in through the serial port
-#It is stored in an ordered dictionary
-class Config:
-    CONFIGSIZE = 44
-    #fmt = 'Hh8h11sxbb5s5s'
-    st = struct.Struct('Hh8h11sxbb5s5s')
-    #keys = ['ints','pulse','A','G','B','L','M','S','V','na','file','mode','channel','txaddr','rxaddr']
-    keys = ['flags','pulse','accel','gyro','baro','light','magneto','sync','voltage','/na','file','mode','channel','txaddr','rxaddr']
+    if out != '':
+        print(out,end='')
+        if not out.endswith('\n'):
+            print('')
 
-    def __init__(self,data):
-        values = Config.st.unpack(data)
-        self.cfg = cs.OrderedDict(zip(Config.keys,values))
+#Wait for data on serial port, timeout of 1 second (10 * .1)
+def wait_serial(ser):
+    i = 10
+    while ser.inWaiting() == 0 and i > 0:
+        i = i - 1
+        time.sleep(.1)
 
-    def __str__(self):
-        pdict = copy.deepcopy(self.cfg)
-        #pdict['txaddr'] = b.hexlify(pdict['txaddr'])
-        #pdict['rxaddr'] = b.hexlify(pdict['rxaddr'])
-        #pdict['flags'] = bin(pdict['flags'])[2:].zfill(8)
-        #pdict['mode'] = bin(pdict['mode'])[2:].zfill(8)
-        string = ''
-        for k,v in pdict.items():
-            string += k + '\t: ' + str(v) + '\n'
-        return string
+#Called in a thread to check for exit character while listening on serial port
+def input_check():
+    threadLock.acquire()
+    while True:
+        ch = readchar.readchar()
+        if (ch == 'q'):
+            break
+    threadLock.release()
 
-    def repack(self):
-        return Config.st.pack(*self.cfg.values())
-
+#Listen on device serial port in a thread, stop when lock releases, 
+#indicating that 'q' was pressed
+def listen():
+    print('Listening: (\'q\' to quit)')
+    save_timeout = ser.timeout
+    ser.timeout = .1
+    while not threadLock.acquire(blocking = False):
+        data = ser.readline()
+        print(data.decode('ascii'),end='')
+        if('outfile' in locals()): #This checks if 'outfile' is declared
+            outfile.write(data.decode('ascii'))
+    print('Stopping...')
+    ser.timeout = save_timeout
 
 
 # configure the serial connections, enumerating valid and available serial ports for
 #the current operating system
-
 print('Select serial port:')
 options = enum_serial_ports()
 for n in range(len(options)):
     print(str(n+1) + ': ' + str(options[n]))
 print(str(len(options) + 1) + ': None of the above, exit')
-n = int(input('>> '))
+
+n = int(input('>> ')) #Get user input to select serial port
+
 if(n-1 == len(options)):
-    exit()
+    exit()  #If none option selected, exit
+
+#TODO add input validation on serial port selection
+
 ser = serial.Serial(
     port=options[n-1],
     baudrate=9600,
@@ -94,9 +124,6 @@ print('Enter your commands below.')
 helpstr = """Commands:
 help\t (h): This
 config\t(cf): Enters Config mode, nothing happens before this
-read\t(rd): Read config from device
-change\t(ch): Inline change loaded config
-write\t(wt): Write loaded config back to device
 commit\t (c): Commit changes to device, resetting it
 listen\t(ls): Print out all data from device
 empty\t (e): If serial buffers are full, run this to empty
@@ -104,105 +131,74 @@ exit\t (x): exits
 Anything else will be sent straight to the device
 """
 print(helpstr)
-ser.timeout = 5
+
+#time (in seconds) before serial read timeout
+ser.timeout = 1
+
 while 1 :
     inp = input(">> ")
 
     if inp == 'exit' or inp == 'x':
         ser.close()
         exit()
-
     elif inp == 'help' or inp == 'h':
         print(helpstr)
-        continue
-
+        continue #skip unnecesary serial read
+#Sends the command to enter config mode
     elif inp == 'config' or inp == 'cf':
         ser.write('config\r'.encode())
-        i = 10
-        while ser.inWaiting() == 0 and i > 0:
-            i = i - 1
-            time.sleep(.1)
-
+        wait_serial(ser)
+#Ends config mode on device, resetting it
     elif inp == 'commit' or inp == 'c':
-        ser.write('commit'.encode())
-        i = 10
-        while ser.inWaiting() == 0 and i > 0:
-            i = i - 1
-            time.sleep(.1)
-
+        ser.write('commit\r'.encode())
+        wait_serial(ser)
+#This command simply outputs enything received over serial to the terminal
     elif 'listen' in inp or inp == 'ls':
-        print('Listening: ')
-        while 1:
-            data = ser.readline()
-            print(data.decode('ascii'),end='')
+        
+        print('Write to file: (enter filename or \'n\' to skip)')
+        filename = input()
+        if not (filename == 'n' or filename == 'no'):
+            outfile = open(filename,'w',-1)
 
-#change read config, dont run before reading!
-    elif inp == 'change' or inp == 'ch':
-        newinp = rlinput('edit config:\n',str(ldcfg))
-        for line in newinp.splitlines():
-            ldcfg.cfg[line[:line.find('\t')]] = ast.literal_eval(line[line.index(':')+2:])
+#Setup threads to listen on port and check user for input; 
+# if 'q' is pressed, stop listening. Threads are only necessary
+# to allow quitting during listen
 
-#Write config back to device, dont run before reading!
-    elif inp == 'write' or inp == 'wt':
-        data = ldcfg.repack()
-        ser.write('wt\r'.encode('ascii'))
-        ser.write(data)
-        data2 = ser.read(Config.CONFIGSIZE)
-        if data2 == data:
-            print('correct')
-            ser.write(bytes([255]))
-        else:
-            print('nope')
-            print(data2)
-            ser.write(bytes([0]))
-        print(ser.readline().decode(),end='')
+#The lock is acquired by input_check thread and released when 
+#'q' is detected.  The listen thread attempts to acquire the lock
+#to check if it should continue listening
+        threadLock = threading.Lock()
+        threads = []
 
-#Read config from device
-    elif inp == 'read' or inp == 'rd':
-        ser.write('rd\r'.encode('ascii'))
-        out = ''
-        i = 10
-        while ser.inWaiting() == 0 and i > 0:
-            i = i - 1
-            time.sleep(.1)
-        while(out != 'read\n#!bin\n'):
-            out += ser.read(1).decode('ascii')
-        byts = ser.read(Config.CONFIGSIZE)
-        while(ser.inWaiting()):
-            ser.read(1)
-        ldcfg = Config(byts)
-        print(ldcfg)
+        inputThread = threading.Thread(target=input_check)
+        listenThread = threading.Thread(target=listen)
 
+        threads.append(inputThread)
+        threads.append(listenThread)
+
+        inputThread.start()
+        listenThread.start()
+
+        for th in threads:
+            th.join()
+
+        continue # I dont know if I should have this; it just skips
+                # an additional read of the port
+        
+#This command empties the serial buffer from the device, but doesnt display it
+#Sometimes the client gets behind what the device is sending, use this to clear
     elif inp == 'empty' or inp == 'e':
         while ser.inWaiting() > 0:
             while ser.inWaiting() > 0:
-                byt = ser.read(1)
+                byt = ser.read(1) #read 1 byte
             time.sleep(.2)
-
 #Send command straight to device.  Will later be deprecated, 
 #as all will be handled by client
     else:
         inp += '\r'
         ser.write(inp.encode('ascii'))
-        out = ''
-        # let's wait one second before reading output (let's give device time to answer)
-        #time.sleep(1)
-        i = 10
-        while ser.inWaiting() == 0 and i > 0:
-            i = i - 1
-            time.sleep(.1)
+        wait_serial(ser)
 #This waiting on the device occurs for every command
 #which is why it is on the level of the while(1)
-    out = ''
-    i = 100
-    while ser.inWaiting() > 0 and i > 0:
-        while ser.inWaiting() > 0 and i > 0:
-            byt = ser.read(1)
-            out += byt.decode('ascii')
-            i -= 1
-        time.sleep(.2)
+    read_serial(ser)
 
-    if out != '':
-        print(out,end='')
-        if not out.endswith('\n'):
-            print('')
