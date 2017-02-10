05a0d745e1458674f17045331aaeb5ae566795fa
Kristian Sims <kristian.sims@gmail.com>
2017-02-09 19:04:10 -0700

    vatcsv.py coming along, need data assmebly from records next

--- vatcsv.py
+++ vatcsv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 
+from sys import stderr
 from motion import Motion
 from pressure import Pressure
 from light import Light
@@ -9,11 +10,12 @@ from light import Light
 def read_chunks(file, size=512):
     try:
         chunk = '-'
-        while chunk:
+        while chunk and not file.closed:
             chunk = file.read(size)
             yield chunk
     finally:
-        file.close()
+        if not file.closed:
+            file.close()
 
 
 def main(file):
@@ -25,9 +27,29 @@ def main(file):
     }
 
     data = b''
-    for chunk in read_chunks(file):
+    for chunk in read_chunks(file, 1024):
         data += chunk
-        while len(data) > 16:
-            tag = data[0]
-            if tag in sensors:
-                sensors[tag].append(data[1:])
+        dex = 0
+        while dex < len(data) - 16:
+
+            if data[dex] in sensors:
+                tag = data[dex]
+                dex += 1
+                sensors[tag].append(data, dex)
+                dex += sensors[tag].length
+            elif tag == 0x75:
+                dex += 5  # FIXME!!!
+            elif tag == 0x58:
+                if data[dex:dex + 9] == b'\0\0\0\0\0\0\0\0q':
+                    file.close()  # FIXME :(
+            elif tag == 0x4C:
+                end = data.find(b'\n', dex)
+                if end > 0:
+                    # result += chr(tag) + data[dex:end + 1].decode()
+                    dex = end + 1
+                else:
+                    dex -= 1
+                    break
+            else:
+                print('Skipping bad tag: <' + hex(data[dex]) + '>')
+                dex += 1
