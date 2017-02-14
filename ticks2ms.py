00eb2fa6e5c18f3507c9407668f39d7ac89f409c
Kristian Sims <kristian.sims@gmail.com>
2017-02-14 16:49:52 -0700

    Little stringy tweaks

--- ticks2ms.py
+++ ticks2ms.py
@@ -13,17 +13,18 @@ def main():
                         default=stdout)
     parser.add_argument('--decimal', '-d', action='store_const',
                         const=True, help='Use decimal time stamps')
-    parser.add_argument('--tag', default='D,Time:0ms',
-                        help='Prepend this to indicate version')
+    parser.add_argument('--spec', default='D,Time:0ms',
+                        help='Spec to indicate version (or "none")')
     parser.add_argument('--conversion', default=1000/512,
                         help='Multiply timestamps by this rate')
     
     args = parser.parse_args()
 
-    print(args.tag, file=args.outfile)
+    if not args.spec.lower() == 'none':
+        print(args.spec, file=args.outfile)
     
     for line in args.infile:
-        if line:
+        if line.strip():  # Filter out empty line
             parts = line.split(',')
 
             if len(parts) > 1 and parts[1].isdigit():
